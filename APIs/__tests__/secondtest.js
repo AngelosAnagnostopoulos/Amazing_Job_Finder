2fd82aed8f77f47b1fe8f2005459c5f0783a93e6
Angelos Anagnostopoulos <angelosanag@protonmail.com>
2022-04-09 15:40:29 +0300

    Maybe now?

---
+++ APIs/__tests__/secondtest.js
@@ -0,0 +1,8 @@
+var assert = require('assert');
+describe('Array', function () {
+    describe('#indexOf()', function () {
+        it('should return the index when the value is not present', function () {
+            assert.equal([3, 4, 5].indexOf(4), 1);
+        });
+    });
+});
